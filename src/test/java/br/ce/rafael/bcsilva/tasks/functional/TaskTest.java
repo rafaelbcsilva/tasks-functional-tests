c98a58efc2f6627d9c1f26f45bd643d48ca028c8
Rafael Bezerra C da Silva <rafael.bcsilva@gmail.com>
2022-07-09 01:55:09 -0300

    Atualização IP

--- src/test/java/br/ce/rafael/bcsilva/tasks/functional/TaskTest.java
+++ src/test/java/br/ce/rafael/bcsilva/tasks/functional/TaskTest.java
@@ -23,7 +23,7 @@ public class TaskTest {
 		DesiredCapabilities cap = DesiredCapabilities.chrome();
 		//ChromeOptions cap = new ChromeOptions();
 		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
-		driver.navigate().to("http://192.168.1.223:8001/tasks");
+		driver.navigate().to("http://192.168.0.103:8001/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 	}
